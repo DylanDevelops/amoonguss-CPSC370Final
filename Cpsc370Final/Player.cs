e8f977ece8efad0b11ead873c73befa59b3d3424
Tom Moore <thommoore@chapman.edu>
2025-01-29 20:52:42 -0800

    Tests not working

---
+++ Cpsc370Final/Player.cs
@@ -0,0 +1,21 @@
+namespace Cpsc370Final;
+
+public static class Player
+{
+    public static int SandiesPerClick {get; private set;}
+
+    public static void Initialize()
+    {
+        SandiesPerClick = 1;
+    }
+
+    public static void Click()
+    {
+        SandieBank.addSandiesToBank(SandiesPerClick);
+    }
+
+    public static void UpdateSandiesPerClick(int sandiesPerClickToAdd)
+    {
+        SandiesPerClick += sandiesPerClickToAdd;
+    }
+}
