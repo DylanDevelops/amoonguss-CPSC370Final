8223cbb8afe78b0dc690aa66adfcd0658eef49a7
AndrewEppich <eppich@chapman.edu>
2025-01-29 20:10:01 -0800

    Random events almost done and some tests written

--- Cpsc370Final/RandomEvent.cs
+++ Cpsc370Final/RandomEvent.cs
@@ -1,14 +1,68 @@
 namespace Cpsc370Final
+using System;
 
 class RandomEvent{
 
-    private static string getRandomEvent(){
+    static string getRandomEvent(){
+        int randomEvent;
+        int numSandies = "placeholder for method"
+        int randomNumber = genRandomNumber();
+        switch(randomNumber){
+            case: 1
+                randomEvent = taxesEvent(numSandies);
+                break;
+            case 2:
+                randomEvent = longerWordEvent();
+                break;
+            case 3:
+                (bool)randomEvent = chocolateEvent();
+                break;
+            case 4:
+                randomEvent = treatEvent(numSandies);
+                break;
+            case 5:
+                randomEvent = walkEvent(numSandies);
+                break;
+        }
         
-        
-        return "placeholder"
+        return randomEvent;
     }
 
-    private static string 
+    private static int taxesEvent(int numSandies){
+        int taxedAmount = (double)numSandies * 0.8;
+        return taxedAmount;
+    }
+
+    private static string longerWordEvent(){
+        return " "
+    }
+
+    private static bool chocolateEvent(){
+        return true;
+    }
+
+    private static int treatEvent(int numSandies){
+        int boostedAmount = (double)numSandies * 1.2;
+        return boostedAmount;
+
+    }
+
+    private static int walkEvent(int numSandies){
+        int boostedAmount = (double)numSandies * 1.2;
+        return boostedAmount;
+
+    }
+
+    private static int genRandomNumber(){
+        Random rnd = new Random();
+        int randomInteger = rnd.Next(1,5);
+        return randomInteger;
+    }
+
+
+
+
+
 
 
 
